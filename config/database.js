a4b980d3110a9c8022f66a09a0a770c9cc3de3df
anxhina <anxhina.gjoni@fti.edu.al>
2020-06-06 17:57:03 +0200

    db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/ulodha',
-    secret: 'motivation'
-    }
-    
-    
+    secret: 'wkgRfx_WBeduG_GSWy95SIEoniGjguS3z83Gy_Iz5A9Il',
+    db_user: 'anxhina',
+    db_psswd: 'anxhina',
+    database: 'cluster0-csnj4.mongodb.net/restaurant'
+}
