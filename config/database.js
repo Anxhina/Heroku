8783f3213c5793f7ee36cdf3c41cc12470e5a7c6
anxhina <anxhina.gjoni@fti.edu.al>
2020-06-06 18:32:42 +0200

    db3

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    secret: 'wkgRfx_WBeduG_GSWy95SIEoniGjguS3z83Gy_Iz5A9Il',
+    secret: 'wkgRfx_WBeduG_GSWy95SIEoniGjguS3z83Gy_Iz5l',
     db_user: 'anxhina',
     db_psswd: 'anxhina',
     database: 'cluster0-csnj4.mongodb.net/restaurant'
