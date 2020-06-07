7e64c2008b1769bb45178d2cf989092aae9b9bc5
anxhina <anxhina.gjoni@fti.edu.al>
2020-06-07 13:00:16 +0200

    db20

--- app.js
+++ app.js
@@ -61,14 +61,15 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // app.use(logger('dev'));
 // app.use(express.json());
+app.use(express.static('/Client/dist/'));
+app.use(bodyParser.json());
+
 app.use(cors());
-app.use(express.static('/Client/dist/Mut2'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/Client/dist/Mut2/index.html'));
+res.sendFile(path.join(__dirname,'/Client/dist/index.html'));
 });
-app.use(bodyParser.json());
 
 app.use(passport.initialize());
 app.use(passport.session());
